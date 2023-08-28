2586802a06dc5e84dfd3acd35bdc29cb29a7e834
Rostyslav Tyshko <tyshko1@gmail.com>
2023-08-28 14:37:42 +0200

    test: `create_the_same_role_twice_should_fail`

--- pallets/rbac/src/tests.rs
+++ pallets/rbac/src/tests.rs
@@ -11,3 +11,21 @@ fn create_role() {
 		System::assert_last_event(Event::RoleCreated { role: Role { pallet: [0; 36],permission: crate::Permission::Execute } }.into());
 	});
 }
+
+#[test]
+fn create_the_same_role_twice_should_fail() {
+	new_test_ext().execute_with(|| {
+		System::set_block_number(1);
+		let pallet_name = [0; 36];
+		let role = Role { pallet: pallet_name,permission: crate::Permission::Execute };
+		// Assert creating the role first time succeeds
+		assert_ok!(TemplateModule::create_role(RuntimeOrigin::signed(1), pallet_name, crate::Permission::Execute));
+		// Assert that the correct event was deposited
+		System::assert_last_event(Event::RoleCreated { role  }.into());
+		// Assert creating the same role second time fails
+		assert_noop!(
+			TemplateModule::create_role(RuntimeOrigin::signed(1), pallet_name, crate::Permission::Execute),
+			crate::pallet::Error::RoleAlreadyExists::<Test>,
+		);
+	});
+}
