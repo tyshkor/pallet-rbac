f921aa748743293da1c9196f5c8b822d272e640f
Rostyslav Tyshko <tyshko1@gmail.com>
2023-08-28 14:35:57 +0200

    bench: `assign_role`

--- pallets/rbac/src/benchmarking.rs
+++ pallets/rbac/src/benchmarking.rs
@@ -19,5 +19,27 @@ mod benchmarks {
 
 	}
 
+	#[benchmark]
+	fn assign_role() {
+		let caller: T::AccountId = whitelisted_caller();
+
+		let role = Role { pallet: [0; 36], permission: crate::Permission::Manage };
+
+		RoleSet::<T>::insert(
+			role.clone(),
+			()
+		);
+
+		PermissionSet::<T>::insert(
+			(caller.clone(), role.clone()),
+			()
+		);
+
+		let account_id: T::AccountId = account("Bob", 3, 3);
+
+		#[extrinsic_call]
+		assign_role(RawOrigin::Signed(caller), account_id, role);
+	}
+
 	impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
 }
