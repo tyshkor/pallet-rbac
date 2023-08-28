0b841deda277956c9ba41062b23ead16bb87d4b9
Rostyslav Tyshko <tyshko1@gmail.com>
2023-08-28 14:35:37 +0200

    bench: `create_role`

--- pallets/rbac/src/benchmarking.rs
+++ pallets/rbac/src/benchmarking.rs
@@ -12,23 +12,11 @@ mod benchmarks {
 	use super::*;
 
 	#[benchmark]
-	fn do_something() {
-		let value = 100u32.into();
+	fn create_role() {
 		let caller: T::AccountId = whitelisted_caller();
 		#[extrinsic_call]
-		do_something(RawOrigin::Signed(caller), value);
+		create_role(RawOrigin::Signed(caller), [0; 36], crate::Permission::Execute);
 
-		assert_eq!(Something::<T>::get(), Some(value));
-	}
-
-	#[benchmark]
-	fn cause_error() {
-		Something::<T>::put(100u32);
-		let caller: T::AccountId = whitelisted_caller();
-		#[extrinsic_call]
-		cause_error(RawOrigin::Signed(caller));
-
-		assert_eq!(Something::<T>::get(), Some(101u32));
 	}
 
 	impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
